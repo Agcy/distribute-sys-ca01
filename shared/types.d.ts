9332fe20f1469bc8e6f24f2f508d6b3df75ec148
Haopeng Liang <765407782@qq.com>
2024-03-10 17:43:29 +0000

    update declaration of data type success

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -38,3 +38,19 @@ export type MovieReviews = {
   rating: number;
 }
 
+// auth relay
+export type SignUpBody = {
+  username: string;
+  password: string;
+  email: string
+}
+
+export type ConfirmSignUpBody = {
+  username: string;
+  code: string;
+}
+
+export type SignInBody = {
+  username: string;
+  password: string;
+}
